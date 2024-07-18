8f84b3ea7c4866390786d248bc655b63f5cb24b5
Sarodwat Shome <sarodwatshome@gmail.com>
2024-07-18 11:00:25 +0530

    Added loggers for all page functions.

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -31,7 +31,6 @@ export const config: Options.Testrunner = {
     //
     specs: [
         // ToDo: define location for spec files here
-        //'./test/features/negative.scenarios.feature'
         './test/features/negative.scenarios.feature'
     ],
     // Patterns to exclude.
@@ -115,7 +114,7 @@ export const config: Options.Testrunner = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: [],
+    services: ['chromedriver'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
